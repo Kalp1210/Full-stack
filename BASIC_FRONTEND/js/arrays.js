7aea440a702788df210faa2d867653190a39ca6e
kalp_1210 <rambhiakalp@gmail.com>
2024-06-21 15:26:55 +0530

    arrays started

---
+++ BASIC_FRONTEND/js/arrays.js
@@ -0,0 +1,15 @@
+let days = ["monday", "tuesday", "wednesday"];
+let colors = ["rad", "blue", "green", "yellow", "black"];
+
+colors[0] = "red";
+// methods
+// push adds to array at the end
+days.push("thursday");
+days.push("friday");
+// pop removes item in array from the end
+days.pop();
+
+// shift removes item in array from the start
+colors.shift();
+// unshift adds to array form the start
+colors.unshift("white");
