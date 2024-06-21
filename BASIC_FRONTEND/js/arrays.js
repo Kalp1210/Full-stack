e09b55d5d04200b75f5da7bcab1ee829dfa1f4ae
kalp_1210 <rambhiakalp@gmail.com>
2024-06-21 23:21:09 +0530

    basic of arrays complete

--- BASIC_FRONTEND/js/arrays.js
+++ BASIC_FRONTEND/js/arrays.js
@@ -13,3 +13,26 @@ days.pop();
 colors.shift();
 // unshift adds to array form the start
 colors.unshift("white");
+
+// concatenating 2 arrays
+let array1 = [1, 12, 34, 56, "cat", "dog", 91, 0];
+let array2 = ["hello", "world", 45, 98, "ggwp"];
+let concatarray = array1.concat(array2);
+let include = array1.includes(12);
+// reverse the array
+array2.reverse();
+
+// slice of array
+let slicearray = array1.slice(2, 6);
+
+// splice syntax: splice(starting index,number of things you wanna delete and thing you want to add after the starting index)
+
+let splicearray1 = array2.splice(2, 1);
+let splicearray2 = array1.splice(5, 0, "animals");
+
+// multidimensional array or nested array
+const game = [
+  [0, 1, 2],
+  ["hello", "hi"],
+  ["bat", 3, 4],
+];
