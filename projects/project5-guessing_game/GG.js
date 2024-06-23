16de556efa312c50a67c6d3a275157c714bc1fda
kalp_1210 <rambhiakalp@gmail.com>
2024-06-23 23:18:26 +0530

    guessing number game complete

--- projects/project5-guessing_game/GG.js
+++ projects/project5-guessing_game/GG.js
@@ -1,4 +1,31 @@
 let maximum = parseInt(prompt("Enter the maximum number!"));
+while (!maximum) {
+  maximum = parseInt(prompt("Enter the maximum number!"));
+}
 
-const targetNUm = Math.floor(Math.random() * maximum) + 1;
-console.log(targetNUm);
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+console.log(targetNum);
+
+let guess = prompt("Enter your first guess! (Type q to quit!) ");
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum) {
+  if (guess === "q") break;
+  guess = parseInt(guess);
+
+  if (guess > targetNum) {
+    guess = prompt("Too High!! Enter a new guess:");
+    attempts++;
+  } else if (guess < targetNum) {
+    guess = prompt("Too low!! Enter a new guess:");
+    attempts++;
+  } else {
+    guess = prompt("Invalid guess! pls ente a mumber or 'q' to quit");
+  }
+}
+
+if (guess === "q") {
+  console.log("OK YOU QUIT!!");
+} else {
+  console.log(`YOU GOT IT!! it took you ${attempts} guesses`);
+}
