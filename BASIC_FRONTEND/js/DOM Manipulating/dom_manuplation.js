1dcd9813ce2f27012ba8ec73015369bb6fca03a6
kalp_1210 <rambhiakalp@gmail.com>
2024-07-03 20:27:34 +0530

    dom manuplation basic completed

--- BASIC_FRONTEND/js/DOM Manipulating/dom_manuplation.js
+++ BASIC_FRONTEND/js/DOM Manipulating/dom_manuplation.js
@@ -62,12 +62,6 @@ const allLinks = document.querySelectorAll("a");
 // //     link.innerText = 'I AM A LINK!!!!'
 // // }
 
-for (let link of allLinks) {
-  link.style.color = "rgb(0, 108, 134)";
-  link.style.textDecorationColor = "magenta";
-  link.style.textDecorationStyle = "wavy";
-}
-
 // // this allows us to use htlm tags
 // document.querySelector("h1").innerHTML = "<i>hello world</i>";
 
@@ -81,3 +75,104 @@ for (let link of allLinks) {
 // let firstLink = document.querySelector("a");
 // console.log(firstLink.href);
 // console.log(firstLink.getAttribute("href"));
+
+// ======================================
+// CHANGING STYLES
+// ======================================
+// isme css wale style nahi dhekenge sirf joh inline styles hai woh dheek te hai
+// const h1 = document.querySelector("h1");
+// console.log(h1.style);
+// console.log((h1.style.color = "green"));
+// console.log((h1.style.fontSize = "3em"));
+
+for (let link of allLinks) {
+  link.style.color = "rgb(0, 108, 134)";
+  link.style.textDecorationColor = "magenta";
+  link.style.textDecorationStyle = "wavy";
+}
+// this will give us all the style the h1 has even in the css
+// console.log(window.getComputedStyle(h1));
+// console.log(window.getComputedStyle(h1).fontSize);
+
+// ======================================
+// CLASSLIST
+// ======================================
+// const h2 = document.querySelector("h2");
+// // console.log(h2.getAttribute("class"));
+
+// // // this is linked with css line 35
+// // // aab h2 joh hai phela wala uska color will change to purple kyu ki css main ek class defined hai as purple
+
+// // console.log(h2.setAttribute("class", "purple"));
+// // console.log(h2.setAttribute("class", "border"));
+// console.log(h2.classList);
+
+// // isse apan multiple class asign karsakte hai at the same time
+// console.log(h2.classList.add("purple", "border"));
+// console.log(h2.classList);
+// console.log(h2.classList.remove("purple"));
+// console.log(h2.classList);
+
+// // contains tells us if the class is there or no
+// console.log(h2.classList.contains("purple"));
+// console.log(h2.classList.toggle("purple"));
+
+// ======================================
+//TRAVERSING PARENT/CHILD/SIBLING
+// ======================================
+
+// const firstBold = document.querySelector("b");
+// console.log(firstBold);
+// console.log(firstBold.parentElement);
+// console.log(firstBold.parentElement.parentElement);
+// console.log(firstBold.parentElement.parentElement.parentElement);
+// const para = firstBold.parentElement;
+// console.log(para.children);
+
+// const firstImg = document.querySelector(".square");
+// console.log(firstImg.nextSibling);
+// console.log(firstImg.previousSibling);
+// // niche wale main we dont consider nodes
+// console.log(firstImg.nextElementSibling);
+
+// ======================================
+//APPEND AND APPENDCHILD
+// ======================================
+
+// const newImg = document.createElement("img");
+// console.log(newImg);
+// console.log(
+//   (newImg.src =
+//     "https://images.unsplash.com/photo-1719952167476-d229531f3dab?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+// );
+// console.log(document.body.appendChild(newImg));
+// console.log(newImg.classList.add("square"));
+
+// const p = document.querySelector("p");
+// console.log(
+//   p.append("!!!!!!!!!!!!!! I am new text yaaaaaaayyyyyyyyyyyy!!!!!!!!!!!!!!!")
+// );
+
+// console.log(
+//   p.prepend("!!!!!!!!!!!!!! I am new text yaaaaaaayyyyyyyyyyyy!!!!!!!!!!!!!!!")
+// );
+
+// const newh2 = document.createElement("h2");
+// console.log(
+//   newh2.append(
+//     "!!!!!!!!!!!!!! I am new text yaaaaaaayyyyyyyyyyyy!!!!!!!!!!!!!!!"
+//   )
+// );
+// const h1 = document.querySelector("h1");
+// h1.insertAdjacentElement("afterend", newh2);
+
+// ======================================
+//APPEND AND APPENDCHILD
+// ======================================
+
+// const firstLi = document.querySelector("li");
+// console.log(firstLi);
+// const ul = firstLi.parentElement;
+// console.log(ul);
+// // ul.removeChild(firstLi);
+// firstLi.remove();
