557fcb5a967f8a3998ee0c61c963b0f1fee4853c
kalp_1210 <rambhiakalp@gmail.com>
2024-07-04 21:20:04 +0530

    DONE TILL RANDOM COLOR GENRATOR

--- BASIC_FRONTEND/js/DOM Manipulating/dom_events.js
+++ BASIC_FRONTEND/js/DOM Manipulating/dom_events.js
@@ -1,5 +1,13 @@
-const btn = document.querySelector("#v2");
-btn.onclick = function () {
+// ======================================
+//           **DOM EVENTS**
+// ======================================
+
+// ======================================
+// ONCLICK PROPERTY
+// ======================================
+
+const btn1 = document.querySelector("#v1");
+btn1.onclick = function () {
   console.log("you clicked me!!");
   alert("it worked because of js");
 };
@@ -7,4 +15,42 @@ function scream() {
   console.log("place the cursor somewhere else");
   //   alert("aaaaahhhhhhhhhh!!!!1");
 }
-btn.onmouseenter = scream;
+btn1.onmouseenter = scream;
+
+// ======================================
+// ADDEVENTLISTENER
+// ======================================
+
+// we can use this multiple times it will not be overwritten
+const btn2 = document.querySelector("#v2");
+// btn2.addEventListener("click", function () {
+//   alert("clicked!");
+// });
+
+function click() {
+  console.log("clicked!");
+}
+function Stop() {
+  console.log("stop clicking");
+}
+// it will only happen once
+btn2.addEventListener("click", click, { once: true });
+btn2.addEventListener("click", Stop);
+
+// ======================================
+// RANDOM COLOR GENERATOR
+// ======================================
+const colorBtn = document.querySelector("#colorChange");
+const colorH1 = document.querySelector("#colorH1");
+colorBtn.addEventListener("click", function () {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  const newColor = `rgb(${r}, ${g}, ${b})`;
+  document.body.style.backgroundColor = newColor;
+  colorH1.innerText = newColor;
+});
+
+// ======================================
+// EVENTS AND KEYWORD THIS
+// ======================================
