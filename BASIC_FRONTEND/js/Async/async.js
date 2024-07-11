f292aa2019cf016f32c2e2a29b7969246fc7a873
kalp_1210 <rambhiakalp@gmail.com>
2024-07-11 17:18:22 +0530

    async complete

--- BASIC_FRONTEND/js/Async/async.js
+++ BASIC_FRONTEND/js/Async/async.js
@@ -2,6 +2,8 @@
 //             **ASYNC**
 // =====================================
 
+// const { timeout, reject, someLimit } = require("async");
+
 //======================================
 //The Call Stack
 // =====================================
@@ -90,6 +92,132 @@ colorChangeBtn.addEventListener("click", function () {
 // })
 
 //======================================
-// Callback Hell
+// Promies
 // =====================================
 // promise intro wale file main hai
+
+//======================================
+// Creating Promies
+// =====================================
+// const fakeRequest = (url) => {
+//   return new Promise((resolve, reject) => {
+//     const rand = Math.random();
+//     setTimeout(() => {
+//       if (rand < 0.7) {
+//         resolve("YOUR FAKE DATA IS HERE");
+//       }
+//       reject("REQUEST ERROR!!");
+//     }, 1000);
+//   });
+// };
+// fakeRequest("/dogs/1")
+//   .then((data) => {
+//     console.log("done with the request!!");
+//     console.log("data is : ", data);
+//   })
+//   .catch((err) => {
+//     console.log("ohhhh nooo!!", err);
+//   });
+
+const promsieColorChange = (color, time) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      document.body.style.backgroundColor = color;
+      resolve();
+    }, time);
+  });
+};
+
+const promiseBtn = document.querySelector("#promiseBtn");
+promiseBtn.addEventListener("click", function () {
+  promsieColorChange("red", 1000)
+    .then(() => {
+      return promsieColorChange("orange", 1000);
+    })
+    .then(() => {
+      return promsieColorChange("yellow", 3000);
+    })
+    .then(() => {
+      return promsieColorChange("blue", 1000);
+    })
+    .then(() => {
+      return promsieColorChange("green", 1000);
+    })
+    .then(() => {
+      return promsieColorChange("white", 1000);
+    });
+});
+
+//======================================
+//The Async Keyword
+// =====================================
+async function hello() {
+  return "hellloooooo";
+}
+console.log(hello());
+
+const sing = async () => {
+  // throw new Error("oohhh no error");
+  // throw "OOHHHH NOOO ERROR!!";
+  return "la la la la ";
+};
+
+console.log(sing());
+sing()
+  .then((data) => {
+    console.log("promise resolved with :", data);
+  })
+  .catch((err) => {
+    console.log("OOHHH NO, PROMISE REJECTED");
+    console.log(err);
+  });
+
+const login = async (username, password) => {
+  if (!username || !password) throw "missing credentials";
+  if (password === "kalpisgreat") return "welcome";
+  throw "invalid password";
+};
+
+login("hello", "kalpisgreat")
+  .then((msg) => {
+    console.log("logged in!");
+    console.log(msg);
+    return login("hello");
+  })
+  .then((msg) => {
+    console.log("logged in!");
+    console.log(msg);
+    return login("hello", "hjaks");
+  })
+  .then((msg) => {
+    console.log("logged in!");
+    console.log(msg);
+  })
+  .catch((errs) => {
+    console.log("error!!");
+    console.log(errs);
+  });
+
+//======================================
+//The await keyowrd
+// =======================================
+
+// yeh same upar bhi hai isliye not writing it again
+// const promsieColorChange = (color, time) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       document.body.style.backgroundColor = color;
+//       resolve();
+//     }, time);
+//   });
+// };
+const awaitBtn = document.querySelector("#awaitBtn");
+
+awaitBtn.addEventListener("click", async function () {
+  await promsieColorChange("red", 1000);
+  await promsieColorChange("orange", 1000);
+  await promsieColorChange("yellow", 1000);
+  await promsieColorChange("blue", 1000);
+  await promsieColorChange("green", 1000);
+  await promsieColorChange("white", 1000);
+});
