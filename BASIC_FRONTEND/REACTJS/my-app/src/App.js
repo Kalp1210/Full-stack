20e5d862c37812f5f1fc214517f60abb7fb37191
kalp_1210 <rambhiakalp@gmail.com>
2024-07-29 23:20:30 +0530

    Die

--- BASIC_FRONTEND/REACTJS/my-app/src/App.js
+++ BASIC_FRONTEND/REACTJS/my-app/src/App.js
@@ -1,18 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "./DieRolls.css";
 import Greeter from "./Greeter";
 import { Dog, add } from "./dog";
 import LoginForm from "./loginForm";
+import Die from "./Die";
+import DiceRolls from "./DiceRolls";
 add(1, 4);
 function App() {
   return (
     <div className="App">
       {/* when we have slef closing tags in react we have to close them like below wala */}
-      <LoginForm />
+      {/* <LoginForm />
       <Greeter />
       <Greeter />
       <Dog />
-      <Dog />
+      <Dog /> */}
+      {/* <Die />
+      <Die />
+      <Die /> */}
+      <DiceRolls className="DieRolls" />
+      <DiceRolls className="DieRolls" />
 
       <header className="App-header">
         <h1>hellllooooo</h1>
